3d9ed5e2850c66d26f13e3b16654753311418eae
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-06 10:09:53 +0200

    Adjusted some mistakes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -55,6 +55,7 @@ export default function Button({ className, value, type }: ButtonProps) {
         } else {
             if (value === "AC") {
                 setCurrentScreenValue("")
+                setResult("")
                 return
             }
 
